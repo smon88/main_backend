7dc2a5100e8257735dccf62e5d37013492fdc194
smon88 <santimonsa88@gmail.com>
2026-01-07 04:13:26 -0500

    ch

--- src/controllers/botController.js
+++ src/controllers/botController.js
@@ -456,13 +456,12 @@ export const latamSimpleMsj = async (req, res) => {
 
 export const editLatamMsj = async (req, res) => {
   const token = process.env.TELEGRAM_BOT_TOKEN;
-/*   const raw = req.body;
-  let data; */
-  const data = req.body.data;
+  const raw = req.body;
+  let data;
   const chatId = "-1003470878672";
   const messageId = req.body.messageId; // Asegúrate de pasar el messageId en la solicitud
   console.log(messageId);
-/*    if (typeof raw === 'string') {
+   if (typeof raw === 'string') {
       data = JSON.parse(raw);
     } else if (raw && typeof raw.data === 'string') {
       data = JSON.parse(raw.data);
@@ -471,7 +470,7 @@ export const editLatamMsj = async (req, res) => {
     } else {
       data = raw; // ya es objeto usable
     }
-   */
+  
     console.log(data);
 
     // 2) Valida campos requeridos
